da2cf2f1810fa371d84cccb374d2f98c8db0e1a7
helloNaresh <naresh4mkola@gmail.com>
2018-06-27 17:01:36 +0530

    modified facultyFetch.java

--- src/main/java/com/example/dbcommunication/bean/FacultyFetch.java
+++ src/main/java/com/example/dbcommunication/bean/FacultyFetch.java
@@ -1,8 +1,12 @@
 package com.example.dbcommunication.bean;
 
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+/* The {@code Integer} class wraps a value of the primitive type
+   {@code int} in an object. An object of type {@code Integer}
+   contains a single field whose type is {@code int}.*/
 @ApiModel
 public class FacultyFetch {
 	@ApiModelProperty(value="This field is mandatory to fill",required=true)
